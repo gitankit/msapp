86368c363e3a4294cee74339e2ac9223e8569c4c
Ankit Sharma <ankit@ankits.in>
2013-02-15 00:22:02 +0530

    Wedding layout with lazy load
    Wedding.html is the mail file to run. Features lazy load and loading of
    content via ajax.

--- ajax.php
+++ ajax.php
@@ -2,6 +2,20 @@
 /**** Sanitize the output and then pass it to AJAX***/
 $getJSON = file_get_contents('http://myshaadi.in/main/blog/ajax_load_more?sort_by=recent&last_page_id=0');
 $encoded_string = (utf8_encode($getJSON));
-header('Content-Type: application/json');
-echo $encoded_string;
+if ( isset ( $_GET['id'] ) ) {
+	$row = $_GET['id'];// - 1;
+	$json = json_decode($encoded_string);
+	if ( $row >= count($json->mag_posts) ) {
+		exit(0); }
+	$html = '<div class="wrap"><img src="'.$json->mag_posts[$row]->MagPost->cover_image.'" >';
+	$html = $html . '<p class="main"><a href="'.$json->mag_posts[$row]->MagPost->link.'">';
+	$html = $html . $json->mag_posts[$row]->MagPost->title .'</a></p><p class="sub"> in ';
+	$html = $html . $json->mag_posts[$row]->MagCategory->name .'</p></div>';
+	header('Content-Type: text/html');
+	echo $html;
+}
+	else {
+			header('Content-Type: application/json');
+			echo $encoded_string;
+}
 ?>
