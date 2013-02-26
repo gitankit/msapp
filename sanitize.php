17cf163e2b4ceeb3995f7114f06aef24aa36f302
Ankit Sharma <ankit@ankits.in>
2013-02-27 02:23:44 +0530

    Update blog.html and sanitize to include categories page

--- sanitize.php
+++ sanitize.php
@@ -1,10 +1,19 @@
 <?php
 /**** Sanitize the output and then pass it to AJAX***/
-if ( !isset($_GET['last_page_id'])) {
+$url = 'http://myshaadi.in/main/blog/ajax_load_more?';
+if ( isset($_GET['last_page_id'])) {
 		//echo "";
-		exit(0);
+		//exit(0);
+		$url = $url.'&last_page_id='.$_GET['last_page_id'];
 }
-$getJSON = file_get_contents('http://myshaadi.in/main/blog/ajax_load_more?sort_by=recent&last_page_id='.$_GET['last_page_id']);
+if ( isset($_GET['mag_category_id'])) {
+		$url = $url.'&mag_category_id='.$_GET['mag_category_id'];
+}
+/*if () {
+	exit(0);
+}*/
+//echo $url;
+$getJSON = file_get_contents($url);
 $encoded_string = $getJSON;//(utf8_encode($getJSON));
 	header('Content-Type: application/json');
 	echo $encoded_string;
