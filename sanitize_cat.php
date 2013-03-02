7a9ee0dd72a2a6b9b92a9fc7cef3d4d77417c3de
Ankit Sharma <ankit@ankits.in>
2013-03-02 11:54:39 +0530

    Added sanitize files to ensure JSON returned thru ajax calls are clean.

---
+++ sanitize_cat.php
@@ -0,0 +1,8 @@
+<?php
+/**** Sanitize the output and then pass it to AJAX***/
+$url = 'http://myshaadi.in/main/blog/api_get_categories?key=sd3fsdva2as';
+$getJSON = file_get_contents($url);
+$encoded_string = $getJSON;//(utf8_encode($getJSON));
+	header('Content-Type: application/json');
+	echo $encoded_string;
+?>
---
+++ sanitize_popular.php
@@ -0,0 +1,16 @@
+<?php
+/**** Sanitize the output and then pass it to AJAX***/
+$url = 'http://myshaadi.in/main/blog/api_get_popular_posts?key=sd3fsdva2as';
+//var_dump($_GET);
+foreach ($_GET as $key => $value) {
+	if($value)
+	{
+		$url = $url.'&'.$key.'='.$value;
+	}
+}
+//echo $url;
+$getJSON = file_get_contents($url);
+$encoded_string = $getJSON;//(utf8_encode($getJSON));
+	header('Content-Type: application/json');
+	echo $encoded_string;
+?>
